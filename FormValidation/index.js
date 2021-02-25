3644dc413747fd4c4066c75204bdc47e76980d5e
dondyprax <juan.acosta7a@gmail.com>
2021-02-24 21:23:56 -0600

    Updating and styling layout

--- FormValidation/index.js
+++ FormValidation/index.js
@@ -1,4 +1,21 @@
 
-function myFunction() {
-    alert("I am alerting!");
+function validateForm() {
+    event.preventDefault();
+    validateName();
+}
+
+function validateName() {
+    let re = /^[A-Za-z]+$/;
+
+    if(document.signupForm.name.value == "") {
+
+        return false;
+    }
+
+    if(!re.test(document.signupForm.name.value)) {
+
+        return false;
+    }
+
+    return true;
 }
