6ac1a68cb89520712c9581afdfef34716773cfd6
dondyprax <juan.acosta7a@gmail.com>
2021-02-26 12:11:28 -0600

    Updating gifs

--- KonamiCode/index.js
+++ KonamiCode/index.js
@@ -1,11 +1,11 @@
 const container = document.getElementById('circle');
 const comboCounter = document.getElementById('combo-counter');
 const calamardo = "a34yc3";
-const banana = "bnxcpt";
-const patrick = "110972";
+const bob = "bnxcpt";
+const patricio = "110972";
 const calamardoCombo = `<img src="https://media.tenor.com/images/a47b9d216da242f48daa59b2bd2dd5c6/tenor.gif" class="gif">`;
-const bananaCombo = `<img src="http://cdn.lowgif.com/full/17e3da5e013b3330-emoji-transparent-gif-on-gifer-by-kadal.gif" class="gif">`;
-const patrickCombo = `<img src="https://i.gifer.com/origin/88/88ee1b0efe8734d6c419e60c3c7f546f_w200.gif" class="gif">`;
+const bobCombo = `<img src="https://static.wikia.nocookie.net/obverse-objects/images/6/68/21FF0565-FC16-47E8-85D4-52D26089B12B.gif/revision/latest/scale-to-width-down/200?cb=20190425194335" class="gif">`;
+const patricioCombo = `<img src="https://i.gifer.com/origin/88/88ee1b0efe8734d6c419e60c3c7f546f_w200.gif" class="gif">`;
 
 let combo = "";
 
@@ -34,20 +34,20 @@ window.addEventListener('keydown', function (e) {
             container.insertAdjacentHTML('afterbegin', calamardoCombo);
             break;
 
-        case banana:
+        case bob:
             if(container.childElementCount > 0) {
                 container.firstChild.remove();
             }
             combo = "";
-            container.insertAdjacentHTML('afterbegin', bananaCombo);
+            container.insertAdjacentHTML('afterbegin', bobCombo);
             break;
 
-        case patrick:
+        case patricio:
             if(container.childElementCount > 0) {
                 container.firstChild.remove();
             }
             combo = "";
-            container.insertAdjacentHTML('afterbegin', patrickCombo);
+            container.insertAdjacentHTML('afterbegin', patricioCombo);
             break;
     }
 }, false);
